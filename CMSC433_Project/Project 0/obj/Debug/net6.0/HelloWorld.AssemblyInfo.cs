3b4cdbe51c9dae45a7d7e34f738593ed3eb2157a
StAnonymousHY <yhanson@terpmail.umd.edu>
2025-01-28 15:43:52 -0500

    1

--- CMSC433_Project/Project 0/obj/Debug/net6.0/HelloWorld.AssemblyInfo.cs
+++ CMSC433_Project/Project 0/obj/Debug/net6.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c05364d53f581f69a8cf78b7e8d7bbefd67b1b96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a132fa8a16fe54e38eac63b85a50ad3e88ce511f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
