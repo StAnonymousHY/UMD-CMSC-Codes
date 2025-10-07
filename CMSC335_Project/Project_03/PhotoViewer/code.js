88aa601aa8f5eeaf16a096cf1802a22a04b64c26
StAnonymousHY <YHXHanson@163.com>
2025-10-06 22:57:24 -0400

    CMSC335 Project03

---
+++ CMSC335_Project/Project_03/PhotoViewer/code.js
@@ -0,0 +1,139 @@
+"use strict";
+
+let curr;
+let loadClicked = false;
+let photoList;
+let slideShowController;
+const interval = 1000;
+
+function loadPhotos() {
+    document.querySelector("#DisplayMessage").textContent = "Photo Viewer System";
+    const folder = document.querySelector("#PhotosFolder").value;
+    const name = document.querySelector("#CommonName").value;
+    let start = document.querySelector("#StartPhotoNumber").value;
+    let end = document.querySelector("#EndPhotoNumber").value;
+
+    if(start > end){
+        document.querySelector("#DisplayMessage").textContent = "Error: Invalid Range";
+        return;
+    }
+
+    loadClicked = true;
+    curr = 0;
+
+    photoList = [];
+    for(let i = start ; i <= end ; i++){
+        photoList.push(folder+name+i+".jpg")
+    }
+
+    document.querySelector("#Image").src = photoList[curr];
+    document.querySelector("#PhotoBeingDisplayed").value = photoList[curr];
+}
+
+async function loadJSON(){
+    document.querySelector("#DisplayMessage").textContent = "Photo Viewer System";
+    const URL = document.querySelector("#URL").value;
+    loadClicked = true;
+    curr = 0;
+
+    const response = await fetch(URL);
+    const json = await response.json();
+
+    photoList = [];
+    for(let i = 0 ; i < json.images.length ; i++){
+        photoList.push(json.images[i].imageURL);
+    }
+
+    document.querySelector("#Image").src = photoList[curr];
+    document.querySelector("#PhotoBeingDisplayed").value = photoList[curr];
+}
+
+function previousPhoto(){
+    if(!loadClicked){
+        document.querySelector("#DisplayMessage").textContent = "Error: you must load data first";
+        return;
+    }
+
+    curr -= 1;
+    if(curr < 0){
+        curr = photoList.length - 1;
+    }
+
+    document.querySelector("#Image").src = photoList[curr];
+    document.querySelector("#PhotoBeingDisplayed").value = photoList[curr];
+}
+
+function nextPhoto(){
+    if(!loadClicked){
+        document.querySelector("#DisplayMessage").textContent = "Error: you must load data first";
+        return;
+    }
+
+    curr += 1;
+    if(curr >= photoList.length){
+        curr = 0;
+    }
+
+    document.querySelector("#Image").src = photoList[curr];
+    document.querySelector("#PhotoBeingDisplayed").value = photoList[curr];
+}
+
+function firstPhoto(){
+    if(!loadClicked){
+        document.querySelector("#DisplayMessage").textContent = "Error: you must load data first";
+        return;
+    }
+
+    curr = 0;
+
+    document.querySelector("#Image").src = photoList[curr];
+    document.querySelector("#PhotoBeingDisplayed").value = photoList[curr];
+}
+
+function lastPhoto(){
+    if(!loadClicked){
+        document.querySelector("#DisplayMessage").textContent = "Error: you must load data first";
+        return;
+    }
+
+    curr = photoList.length - 1;
+
+    document.querySelector("#Image").src = photoList[curr];
+    document.querySelector("#PhotoBeingDisplayed").value = photoList[curr];
+}
+
+function slideShow(){
+    if(!loadClicked){
+        document.querySelector("#DisplayMessage").textContent = "Error: you must load data first";
+        return;
+    }
+
+    clearInterval(slideShowController);
+    /* LAMBDA */
+    slideShowController = setInterval(() => nextPhoto(), interval);
+}
+
+function randomSlideShow(){
+    if(!loadClicked){
+        document.querySelector("#DisplayMessage").textContent = "Error: you must load data first";
+        return;
+    }
+
+    clearInterval(slideShowController);
+    /* LAMBDA */
+    slideShowController = setInterval(() => {
+        curr = Math.floor(Math.random() * photoList.length); 
+        document.querySelector("#Image").src = photoList[curr];
+        document.querySelector("#PhotoBeingDisplayed").value = photoList[curr];
+        console.log(curr);
+    }, interval);
+}
+
+function stopSlideShow(){
+    if(!loadClicked){
+        document.querySelector("#DisplayMessage").textContent = "Error: you must load data first";
+        return;
+    }
+
+    clearInterval(slideShowController);
+}
